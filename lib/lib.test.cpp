6906f88dd129d23a4aeb5a98b44ae6e387672994
XzoRit <xzorit@hotmail.com>
2019-06-15 00:46:20 +0200

    make test data test-global

--- lib/lib.test.cpp
+++ lib/lib.test.cpp
@@ -5,30 +5,37 @@
 #include <vector>
 
 BOOST_AUTO_TEST_CASE(multidim_flat_view)
+const std::vector<int> oneDim{ { 1, 22, 333 } };
+const std::vector<int> oneDimFlat{ { 1, 22, 333 } };
+const std::vector<std::vector<int>> twoDim{ { 1, 22, 333 },
+                                            { 1, 22, 333 },
+                                            { 1, 22, 333 } };
+const std::vector<int> twoDimFlat{ { 1, 22, 333, 1, 22, 333, 1, 22, 333 } };
+const std::vector<std::vector<std::vector<int>>> threeDim{
+  { { 1, 22, 333 }, { 1, 22, 333 } },
+  { { 1, 22, 333 }, { 1, 22, 333 } },
+  { { 1, 22, 333 }, { 1, 22, 333 } }
+};
+const std::vector<int> threeDimFlat{
+  { 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333 }
+};
+
+BOOST_AUTO_TEST_CASE(dimensionality)
+BOOST_AUTO_TEST_CASE(flat_view)
 {
   {
-    const std::vector<int> a{ { 1, 22, 333 } };
-    const auto& flatView{ multidim::makeFlatView(a) };
-    BOOST_TEST(std::equal(begin(a), end(a), begin(flatView), end(flatView)));
+    const auto& flatView{ multidim::makeFlatView(oneDim) };
+    BOOST_TEST(std::equal(
+      begin(oneDimFlat), end(oneDimFlat), begin(flatView), end(flatView)));
   }
   {
-    const std::vector<std::vector<int>> a{ { 1, 22, 333 },
-                                           { 1, 22, 333 },
-                                           { 1, 22, 333 } };
-    const std::vector<int> b{ { 1, 22, 333, 1, 22, 333, 1, 22, 333 } };
-    const auto& flatView{ multidim::makeFlatView(a) };
-    BOOST_TEST(std::equal(begin(b), end(b), begin(flatView), end(flatView)));
+    const auto& flatView{ multidim::makeFlatView(twoDim) };
+    BOOST_TEST(std::equal(
+      begin(twoDimFlat), end(twoDimFlat), begin(flatView), end(flatView)));
   }
   {
-    const std::vector<std::vector<std::vector<int>>> a{
-      { { 1, 22, 333 }, { 1, 22, 333 } },
-      { { 1, 22, 333 }, { 1, 22, 333 } },
-      { { 1, 22, 333 }, { 1, 22, 333 } }
-    };
-    const std::vector<int> b{
-      { 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333, 1, 22, 333 }
-    };
-    const auto& flatView{ multidim::makeFlatView(a) };
-    BOOST_TEST(std::equal(begin(b), end(b), begin(flatView), end(flatView)));
+    const auto& flatView{ multidim::makeFlatView(threeDim) };
+    BOOST_TEST(std::equal(
+      begin(threeDimFlat), end(threeDimFlat), begin(flatView), end(flatView)));
   }
 }
